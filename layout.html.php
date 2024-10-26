0fb6e4d44deaa60eb566db936f577bc146f21c69
arifbdarsono <71606151+arifbdarsono@users.noreply.github.com>
2024-10-27 00:38:28 +0700

    Create layout.html.php
    First init

---
+++ layout.html.php
@@ -0,0 +1,223 @@
+<?php if (!defined('HTMLY')) die('HTMLy'); ?>
+<!DOCTYPE html>
+<html lang="<?php echo blog_language();?>">
+<head>
+    <?php echo head_contents();?>
+    <title><?php echo $title;?></title>
+    <meta name="description" content="<?php echo $description; ?>"/>
+    <link rel="canonical" href="<?php echo $canonical; ?>" />
+    <?php echo $metatags;?>
+    <!-- Bootstrap Core CSS -->
+    <link href="<?php echo theme_path();?>css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="<?php echo theme_path();?>css/clean-blog.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<?php if (facebook()) { echo facebook(); } ?>
+<?php if (login()) { toolbar(); } ?>
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="<?php echo site_url();?>"><?php if(config('navbar.brand') == ''){echo blog_title();} else {echo config('navbar.brand');}?></a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <?php echo menu('navbar-nav navbar-right') ?>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+    <!-- Page Header -->
+    <!-- Set your background image for this header on the line below. -->
+    <header class="intro-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <?php if (isset($is_front)):?>
+                        <div class="site-heading">
+                            <h1><?php echo blog_title();?></h1>
+                            <hr class="small">
+                            <span class="subheading"><?php echo blog_tagline();?></span>
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.homebg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.homebg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_post)):?>
+                        <div class="post-heading">
+                            <h1><?php echo $p->title;?></h1>
+                            <span class="meta"><?php echo i18n('Posted_in');?> <?php echo $p->category;?> <?php echo i18n('by');?> <a href="<?php echo $p->authorUrl;?>"><?php echo $p->authorName;?></a> - <?php echo format_date($p->date); ?></span>
+                        </div>
+                        <style>
+                        <?php if(empty($p->image)) {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/post-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo $p->image;?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_page) || isset($is_subpage)):?>
+                        <div class="page-heading">
+                            <h1><?php echo $p->title;?></h1>
+                            <hr class="small">
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.pagebg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.pagebg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_profile)):?>
+                        <div class="page-heading">
+                            <h1><?php echo $name ?></h1>
+                            <hr class="small">
+                            <span class="subheading"><?php echo $about; ?></span>
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.profilebg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/about-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.profilebg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_blog)):?>
+                        <div class="page-heading">
+                            <h1>Blog</h1>
+                            <hr class="small">
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.blogbg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/post-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.blogbg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_category)):?>
+                        <div class="page-heading">
+                            <h1><?php echo $category->title;?></h1>
+                            
+                            <?php if (config('category.info') === 'true'):?>
+                                <?php if (!empty($category)): ?>
+                                    <hr class="small">
+                                    <span class="subheading">                                   
+                                        <?php echo $category->body; ?>
+                                    </span>
+                                <?php endif; ?>
+                            <?php endif; ?>
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.categorybg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.categorybg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_tag)):?>
+                        <div class="site-heading">
+                            <h1><?php echo $tag->title;?></h1>
+                            <hr class="small">
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.tagbg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.tagbg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_archive)):?>
+                        <div class="site-heading">
+                            <h1><?php echo $archive->title;?></h1>
+                            <hr class="small">
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.archivebg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.archivebg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_search)):?>
+                        <div class="site-heading">
+                            <h1><?php echo $search->title;?></h1>
+                            <hr class="small">
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.searchbg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.searchbg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (isset($is_404) || isset($is_404search)):?>
+                        <div class="site-heading">
+                            <h1>Page Not Found!</h1>
+                            <hr class="small">
+                        </div>
+                        <style>
+                        <?php if(config('cleanblog.404bg') == '') {?>
+                            .intro-header {background-image: url('<?php echo theme_path();?>img/home-bg.jpg')}
+                        <?php } else { ?>
+                            .intro-header {background-image: url('<?php echo config('cleanblog.404bg');?>')}
+                        <?php } ?>
+                        </style>
+                    <?php endif;?>
+                    <?php if (!empty($breadcrumb)): ?>
+                        <div class="breadcrumb <?php if (isset($is_post)):?>left<?php endif;?>"><?php echo $breadcrumb ?></div>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+    </header>
+    <?php echo content();?>
+    <hr>
+    <!-- Footer -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <?php echo social('text-center');?>
+                    <div class="copyright text-muted"><?php echo copyright();?></div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- jQuery -->
+    <script src="<?php echo theme_path();?>js/jquery.min.js"></script>
+    <!-- Bootstrap Core JavaScript -->
+    <script src="<?php echo theme_path();?>js/bootstrap.min.js"></script>
+    <!-- Custom Theme JavaScript -->
+    <script src="<?php echo theme_path();?>js/clean-blog.js"></script>
+    <?php if (analytics()): ?><?php echo analytics() ?><?php endif; ?>
+</body>
+</html>
